31f783c1db596d392973d25e998d9e1fc3146212
fragosojp <joaopaulosoaresfragoso@gmail.com>
2023-02-16 17:02:10 -0300

    feat: add tema claro e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
+import { ThemeProvider } from "@mui/material";
+import { LighTheme } from "./shared/themes";
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <ThemeProvider theme={LighTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };

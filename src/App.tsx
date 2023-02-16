fbbfc3ee786097c67f208f66f28d5aa099a3e158
fragosojp <joaopaulosoaresfragoso@gmail.com>
2023-02-16 17:43:11 -0300

    feat: add tema dark config troca de temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
-import { ThemeProvider } from "@mui/material";
-import { LighTheme } from "./shared/themes";
+import { AppThemeProvider } from "./shared/contexts";
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LighTheme}>
+    <AppThemeProvider>
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
-    </ThemeProvider>
+    </AppThemeProvider>
   );
 };
---
+++ src/shared/contexts/ThemeContext.tsx
@@ -0,0 +1,57 @@
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
+import { DarkTheme, LighTheme } from "../themes";
+import { ThemeProvider } from "@mui/material";
+import { Box } from "@mui/system";
+
+interface IThemeContextData {
+  themeName: "light" | "dark";
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext({} as IThemeContextData);
+
+export const useAppThemeContext = () => {
+  return useContext(ThemeContext);
+};
+
+interface IAppThemeProviderProps {
+  children: React.ReactNode;
+}
+
+export const AppThemeProvider: React.FC<IAppThemeProviderProps> = ({
+  children,
+}) => {
+  const [themeName, setThemeName] = useState<"light" | "dark">("light");
+
+  const toggleTheme = useCallback(() => {
+    setThemeName((oldThemeName) =>
+      oldThemeName === "light" ? "dark" : "light"
+    );
+  }, []);
+
+  const theme = useMemo(() => {
+    if (themeName === "light") return LighTheme;
+
+    return DarkTheme;
+  }, [themeName]);
+
+  return (
+    <ThemeContext.Provider value={{ themeName, toggleTheme }}>
+      <ThemeProvider theme={theme}>
+        <Box
+          width="100vw"
+          height="100vh"
+          bgcolor={theme.palette.background.default}
+        >
+          {children}
+        </Box>
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
+};

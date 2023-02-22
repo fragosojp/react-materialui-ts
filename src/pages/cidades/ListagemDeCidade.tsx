f4e0e1399ea1bb564c6a2192dc150d1548107ea8
fragosojp <joaopaulosoaresfragoso@gmail.com>
2023-02-22 11:15:22 -0300

    Feat: useSearchParams:react-router-dom em busca

--- src/pages/cidades/ListagemDeCidade.tsx
+++ src/pages/cidades/ListagemDeCidade.tsx
@@ -1,12 +1,28 @@
+import { useMemo } from "react";
+import { useSearchParams } from "react-router-dom";
 import { FerramentasDaListagem } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts";
 
 export const ListagemDeCidade: React.FC = () => {
+  //Hooks
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const busca = useMemo(() => {
+    return searchParams.get("busca") || "";
+  }, [searchParams]);
+
   return (
     <LayoutBaseDePagina
       titulo="Listagem de cidades"
       barraDeFerramentas={
-        <FerramentasDaListagem textoBotaoNovo="Nova" mostrarInputBusca />
+        <FerramentasDaListagem
+          mostrarInputBusca
+          textoBotaoNovo="Nova"
+          textoDaBusca={busca}
+          aoMudarTextoDeBusca={(texto) =>
+            setSearchParams({ busca: texto }, { replace: true })
+          }
+        />
       }
     >
       -

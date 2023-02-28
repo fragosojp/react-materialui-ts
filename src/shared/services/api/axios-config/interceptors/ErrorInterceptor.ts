4364c3d2aa82ef539344afc485afc6110f74f08e
fragosojp <joaopaulosoaresfragoso@gmail.com>
2023-02-28 13:10:29 -0300

    Feat: ajuste rota api jsonServe

--- src/shared/services/api/axios-config/interceptors/ErrorInterceptor.ts
+++ src/shared/services/api/axios-config/interceptors/ErrorInterceptor.ts
@@ -2,7 +2,7 @@ import { AxiosError } from "axios";
 
 export const errorInterceptor = (error: AxiosError) => {
   if (error.message === "Network Error") {
-    return Promise.reject(new Error("Erro de conexão. " + error.message));
+    return Promise.reject(new Error("Erro de conexão. "));
   }
 
   if (error.response?.status === 401) {

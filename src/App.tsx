a9fddb37162b5778d6f386860353aaa5d5f0ad3b
fragosojp <joaopaulosoaresfragoso@gmail.com>
2023-03-02 18:34:42 -0300

    Feat: traduzir as msg de validação yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./routes";
-import { MenuLateral } from "./shared/components";
+
+import "./shared/forms/TraducoesYup";
 import { AppThemeProvider, DrawerProvider } from "./shared/contexts";
+import { MenuLateral } from "./shared/components";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
